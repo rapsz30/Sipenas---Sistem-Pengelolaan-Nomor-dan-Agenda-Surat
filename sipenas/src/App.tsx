bd577562d977fc62dc60ea10810c7c9efd64594a
rapsz30 <rafihendryansyah26@gmail.com>
2026-02-18 11:51:46 +0700

    update navigasi

--- sipenas/src/App.tsx
+++ sipenas/src/App.tsx
@@ -1,12 +1,21 @@
 // src/App.tsx
-import Login from './pages/Login/Login'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from './pages/Login/Login';
+import ForgotPass from './pages/ForgotPass/ForgotPass';
+import ForgotUsn from './pages/ForgotUsername/ForgotUsn';
+import Privacy from './pages/Privacy/Privacy'; 
 
 function App() {
   return (
-    <>
-      <Login />
-    </>
+    <BrowserRouter>
+      <Routes>
+         <Route path="/" element={<Login />} />
+         <Route path="/forgot-password" element={<ForgotPass />} />
+         <Route path="/forgot-username" element={<ForgotUsn />} />
+         <Route path="/privacy" element={<Privacy />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
-export default App
+export default App;
